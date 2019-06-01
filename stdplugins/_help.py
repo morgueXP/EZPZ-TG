e8f27343ed1e80685452838ae3553d3861180711
jaskaranSM <37726998+jaskaranSM@users.noreply.github.com>
2019-06-01 08:26:05 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -2,7 +2,6 @@ import sys
 from telethon import events, functions, __version__
 from uniborg.util import admin_cmd
 
-
 @borg.on(admin_cmd(pattern="helpme ?(.*)", allow_sudo=True))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
@@ -12,7 +11,7 @@ async def _(event):
         s_help_string = borg._plugins[splugin_name].__doc__
     else:
         s_help_string = ""
-    help_string = """
+    help_string = """%%%BOT%%%
 Python {}
 Telethon {}
 """.format(
