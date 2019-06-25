ff713765bd4ed167c659a588c5b0523f924e28cc
jaskaranSM <37726998+jaskaranSM@users.noreply.github.com>
2019-06-25 08:43:31 +0530

    Added Github_access_token var.

--- sample_config.py
+++ sample_config.py
@@ -74,6 +74,7 @@ class Config(object):
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
     # Set to True if you want to block users that are spamming your PMs.
     SLAP_USERNAME = os.environ.get("SLAP_USERNAME", None)
+    GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
     NO_P_M_SPAM = bool(os.environ.get("NO_P_M_SPAM", False))
     # define "spam" in PMs
     MAX_FLOOD_IN_P_M_s = int(os.environ.get("MAX_FLOOD_IN_P_M_s", 3))
