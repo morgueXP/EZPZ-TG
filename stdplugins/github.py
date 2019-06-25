24e3027c9b5fd7e3e072de5e7c3801d2199a29c4
jaskaranSM <37726998+jaskaranSM@users.noreply.github.com>
2019-06-25 09:16:00 +0530

    Pushed Initial Version of Github Uploader

--- stdplugins/github.py
+++ stdplugins/github.py
@@ -1,42 +1,85 @@
-"""Get information about an user on GitHub
-Syntax: .github USERNAME"""
+"""
+GITHUB File Uploader Plugin for userbot. Heroku Automation should be Enabled. Else u r not that lazy // For lazy people
+usage:- .commit reply_to_any_plugin //can be any type of file too. but for plugin must be in .py 
+By:- @Zero_cool7870 
+
+"""
+
+
+from github import Github
+import aiohttp
+import asyncio
+import os
+import time
+from datetime import datetime
 from telethon import events
-import requests
-from uniborg.util import admin_cmd
+from telethon.tl.types import DocumentAttributeVideo
+from uniborg.util import admin_cmd, humanbytes, progress, time_formatter
 
 
-@borg.on(admin_cmd("github (.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    input_str = event.pattern_match.group(1)
-    url = "https://api.github.com/users/{}".format(input_str)
-    r = requests.get(url)
-    if r.status_code != 404:
-        b = r.json()
-        avatar_url = b["avatar_url"]
-        html_url = b["html_url"]
-        gh_type = b["type"]
-        name = b["name"]
-        company = b["company"]
-        blog = b["blog"]
-        location = b["location"]
-        bio = b["bio"]
-        created_at = b["created_at"]
-        await borg.send_file(
-            event.chat_id,
-            caption="""Name: [{}]({})
-Type: {}
-Company: {}
-Blog: {}
-Location: {}
-Bio: {}
-Profile Created: {}""".format(name, html_url, gh_type, company, blog, location, bio, created_at),
-            file=avatar_url,
-            force_document=False,
-            allow_cache=False,
-            reply_to=event
-        )
-        await event.delete()
-    else:
-        await event.edit("`{}`: {}".format(input_str, r.text))
+GIT_TEMP_DIR = "./temp/"
+@borg.on(admin_cmd(pattern="commit ?(.*)", allow_sudo=True))
+async def download(event):
+	if event.fwd_from:
+		return	
+	if Config.GITHUB_ACCESS_TOKEN is None:
+		await event.edit("`Please ADD Proper Access Token from github.com`") 
+		return   
+	if Config.GIT_REPO_NAME is None:
+		await event.edit("`Please ADD Proper Github Repo Name of your userbot`")
+	mone = await event.reply("Processing ...")
+	input_str = event.pattern_match.group(1)
+	if not os.path.isdir(GIT_TEMP_DIR):
+		os.makedirs(GIT_TEMP_DIR)
+	start = datetime.now()
+	reply_message = await event.get_reply_message()
+	try:
+		c_time = time.time()
+		downloaded_file_name = await borg.download_media(
+			reply_message,
+			GIT_TEMP_DIR,
+			progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+			progress(d, t, mone, c_time, "trying to download")
+			)
+		)
+	except Exception as e: 
+		await mone.edit(str(e))
+	else:
+		end = datetime.now()
+		ms = (end - start).seconds
+		await mone.edit("Downloaded to `{}` in {} seconds.".format(downloaded_file_name, ms))
+		sleep(1)
+		await mone.edit("Commiting to Github....")
+		git_commit(downloaded_file_name)
+
+
+
+
+
+async def git_commit(file_name):        
+	content_list = []
+	access_token = Config.GITHUB_ACCESS_TOKEN
+	g = Github(access_token)
+	file = open(file_name,"r",encoding='utf-8')
+	commit_data = file.read()
+	repo = g.get_repo(Config.GIT_REPO_NAME)
+	print(repo.name)
+	create_file = True
+	#repo.create_file("test.txt", "test",commit_data, branch="master")
+	contents = repo.get_contents("")
+	for content_file in contents:
+		content_list.append(str(content_file))
+		#print(content_file)
+	for i in content_list:
+		create_file = True
+		if i == 'ContentFile(path="'+file_name+'")':
+			return await mone.edit("File Already Exists")
+			create_file = False
+	file_name = "stdplugins/"+file_name		
+	if create_file == True:		
+		repo.create_file(file_name, file_name, commit_data, branch="master")
+	else:
+		return await mone.edit("Cannot commit")
+
+
+	
