dced24c39d4830181bde066f89a8849947ce76e2
jaskaranSM <37726998+jaskaranSM@users.noreply.github.com>
2019-07-01 15:05:47 +0530

    Get Folder Link Directly

--- stdplugins/upload_to_gDrive.py
+++ stdplugins/upload_to_gDrive.py
@@ -173,3 +173,10 @@ def upload_file(http, file_path, file_name, mime_type):
     file = drive_service.files().get(fileId=file["id"]).execute()
     download_url = file.get("webContentLink")
     return download_url
+
+@borg.on(admin_cmd(pattern="gfolder ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    folder_link = "https://drive.google.com/drive/u/2/folders/"+parent_id    
+    await event.edit("`Here is Your Gdrive Folder link : `\n"+folder_link)    
