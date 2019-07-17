1a836114c4f153cdbb33b926940cf3614ca4ea50
jaskaranSM <37726998+jaskaranSM@users.noreply.github.com>
2019-07-17 15:35:01 +0530

    Update batch_upload.py

--- stdplugins/batch_upload.py
+++ stdplugins/batch_upload.py
@@ -22,19 +22,19 @@ async def batch_upload(event):
 	if Config.TEMP_DIR is None:
 		await event.edit("Please Set Temp Directory.")
 		return
-	channel = -1001301776435
+	in_channel = Config.CHANNEL_ID
 	temp_dir = Config.TEMP_DIR	
 	if os.path.exists(temp_dir):    
 		os.chdir(temp_dir)
 		files = os.listdir()
 		files.sort()
-		print(channel)
+		print(in_channel)
 		await event.edit("Uploading Files on Telegram...")
 		for file in files:
-			required_file_name = "../anime/"+file
+			required_file_name = temp_dir+"/"+file
 			print(required_file_name)
 			await borg.send_file(
-					channel,
+					in_channel,
 					required_file_name,
 					force_document=True
 				)
