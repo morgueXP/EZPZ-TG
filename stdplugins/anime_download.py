c31ca0b56d351f2d1decfca1321d8d2a6f75e6ff
jaskaranSM <37726998+jaskaranSM@users.noreply.github.com>
2019-07-19 16:34:55 +0530

    anime_downloader: Updated Scraping Logic

--- stdplugins/anime_download.py
+++ stdplugins/anime_download.py
@@ -1,7 +1,7 @@
 """
 Anime Batch Downloader Plugin for userbot. //set TEMP_DIR Env Var first.
-usage:- get a link of Animefrenzy.net Anime page and note number of eps they have.
-cmd:- .anime num_of_eps page_link
+usage:- get a link of Animefrenzy.net Anime page and use in cmd.
+cmd:- .anime page_link
 By:- @Zero_cool7870	  
 
 """
@@ -11,7 +11,6 @@ from bs4 import BeautifulSoup as bs
 import requests
 import os
 chunk_size =  3242880
-
 
 
 async def get_file_name(link):
@@ -32,20 +31,10 @@ async def download_file(url,filename):
 			handle.write(chunk)
 	handle.close()   
 
-async def get_download_links(download_link):
-	size = len(download_link)
-	lenk = ""
-	count = 0
-	for i in range(0,size):
-		if i == size - 1:
-			break
-		lenk = lenk + download_link[count]
-		count = count + 1
-	return lenk	
-
 @borg.on(events.NewMessage(pattern=r"\.anime", outgoing=True))
 async def anime_download(event):
 	urls = []
+	url_links = []
 	if event.fwd_from:
 		return   
 	if Config.TEMP_DIR is None:
@@ -58,21 +47,22 @@ async def anime_download(event):
 		pass	
 	
 	var = event.text
-	number_of_eps = var[7:9]
-	number_of_eps = int(number_of_eps)
-	number_of_eps = number_of_eps + 1 
-	download_link = var[10:]  
-	download_link = download_link[30:]
-	download_link = "https://animefrenzy.net/"+download_link
-	download_link = await get_download_links(download_link)
-	await event.edit("Getting Download links...")
-	for i in range(1,int(number_of_eps)):
-		link = download_link+"-episode-"+str(i)+"/"
-		res = requests.get(link)
+	var = var[6:]
+	res = requests.get(var)
+	source = bs(res.text,'lxml')
+
+	for a in source.find_all('a',{'class':'infovan'}):
+		url_links.append(a['href'])
+
+	for i in url_links:
+		res = requests.get(i)
 		source = bs(res.text,'lxml')
+
 		for a in source.find_all('a',{'class':'an'}):
-			urls.append(a['href'])	
-		
+			urls.append(a['href'])
+		print("Getting Link...")	
+			
+	
 	counter = 0
 	for url in urls:
 		if "download.php?" in url:
@@ -85,6 +75,7 @@ async def anime_download(event):
 			urls.pop(counter)	
 		counter = counter + 1 
 	await event.edit("Downloading Episodes...")
+	
 	for i in urls:
 		filename = await get_file_name(i)
 		print(filename)
