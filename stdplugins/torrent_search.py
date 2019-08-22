273c7fd3cdcb3615eeea4945f9c28269a3303b72
jaskaranSM <37726998+jaskaranSM@users.noreply.github.com>
2019-08-22 20:34:47 +0530

    torrent_search: Updated Formatting and Using del.dog now

--- stdplugins/torrent_search.py
+++ stdplugins/torrent_search.py
@@ -1,21 +1,30 @@
 """
 Torrent Search Plugin for Userbot. //torrentdownloads.me
 cmd: .search search_string
-Note: Number of results are currently limited to 20
+Note: Number of results are currently limited to 11
 By:-@Zero_cool7870
 
 """
 from bs4 import BeautifulSoup as bs 
 import requests
-from telethon import events
 from uniborg.util import admin_cmd
 import asyncio
-from telegraph import Telegraph
-
-telegraph = Telegraph()
-telegraph.create_account(short_name='{}'.format(borg.me.username))
+import json
 
 
+
+def dogbin(magnets):
+	counter = 0
+	urls = []
+	while counter != len(magnets):
+		message = magnets[counter]
+		url = "https://del.dog/documents"
+		r = requests.post(url, data=message.encode("UTF-8")).json()
+		url = f"https://del.dog/{r['key']}"
+		urls.append(url)
+		counter = counter + 1
+	return urls	
+	
 @borg.on(admin_cmd(pattern="search ?(.*)", allow_sudo=True))
 async def tor_search(event):
 	if event.fwd_from:
@@ -35,51 +44,54 @@ async def tor_search(event):
 		res = requests.get("https://www.torrentdownloads.me/search/?search="+search_str,headers)
 
 	source = bs(res.text,'lxml')
-	with open("source.html",'w') as f:
-		f.write(str(source))
-
 	urls = []
 	magnets = []
-
+	titles = []
 	counter = 0
-	for a in source.find_all('a',{'class':'cloud'}):
+	for div in source.find_all('div',{'class':'grey_bar3 back_none'}):
 		# print("https://www.torrentdownloads.me"+a['href'])
 		try:
-			urls.append("https://www.torrentdownloads.me"+a['href'])
-		except:
+			title = div.p.a['title']
+			title = title[20:]
+			titles.append(title)
+			urls.append("https://www.torrentdownloads.me"+div.p.a['href'])
+		except KeyError:
 			pass
-		if counter == 19:
+		except TypeError:
+			pass
+		except AttributeError:
+			pass	
+		if counter == 11:
 			break		
 		counter = counter + 1
 	if not urls:
 		await event.edit("Either the Keyword was restricted or not found..")		
 		return
 
-	print("Found URLS....")
-		
+	print("Found URLS...")
 	for url in urls:
 		res = requests.get(url,headers)
 		# print("URl: "+url)
 		source = bs(res.text,'lxml')
-		with open("source2.html",'w') as f:
-			f.write(str(source))
 		for div in source.find_all('div',{'class':'grey_bar1 back_none'}):
 			try:
 				mg = div.p.a['href']
-				# print(str(mg))
-				magnets.append("<b>URL: </b>"+str(url)+"<br/><b>\nMagnet: </b>{}\n<br/>".format(str(mg)))
-			except:
+				magnets.append(mg)
+			except Exception as e:
 				pass	
-	print("Found Magnets...")			
+	print("Found Magnets...")
+	shorted_links = dogbin(magnets)
+	print("Dogged Magnets to del.dog...")
 	msg = ""
 	try:
 		search_str = search_str.replace("+"," ")
 	except:
 		pass	
-	for i in magnets:
-		msg = msg + i
-	response = telegraph.create_page(
-	search_str,
-	html_content = msg
-	)	
-	await event.edit('Magnet Links for {}:\nhttps://telegra.ph/{}'.format(search_str,response['path']))
+	msg = "**Torrent Search Query**\n`{}`".format(search_str)+"\n**Results**\n"
+	counter = 0
+	while counter != len(titles):
+		msg = msg + "⁍ [{}]".format(titles[counter])+"({})".format(shorted_links[counter])+"\n\n"
+		counter = counter + 1
+
+
+	await event.edit(msg,link_preview=False)
