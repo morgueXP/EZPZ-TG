1a603de42b6d2ea1479c56d1f529c81b5a34a58f
jaskaranSM <37726998+jaskaranSM@users.noreply.github.com>
2019-08-31 17:22:41 +0530

    Config: Added MongoDB Connection String var.

--- sample_config.py
+++ sample_config.py
@@ -101,6 +101,8 @@ class Config(object):
     VERY_STREAM_KEY = os.environ.get("VERY_STREAM_KEY", None)
     TEMP_DIR = os.environ.get("TEMP_DIR", None)
     CHANNEL_ID = int(os.environ.get("CHANNEL_ID", None))
+    #MongoDB
+    MONGO_URI = os.environ.get("MONGO_URI", None)
     #Google Chrome Stuff
     CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
     GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
