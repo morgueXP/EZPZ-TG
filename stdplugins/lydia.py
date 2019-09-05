4ae8ca8eacaed72d8e6c968b531f6d4b07c4e5b2
jaskaranSM <37726998+jaskaranSM@users.noreply.github.com>
2019-09-05 19:21:20 +0530

    lydia: Added list Current Users in AI Support and some fixes.

--- stdplugins/lydia.py
+++ stdplugins/lydia.py
@@ -3,10 +3,11 @@ Lydia AI plugin for userbot.
 
 Description: A module that Act as a chatbot and chat with a User/other Bot.
  			This Module Needs CoffeeHouse API to work. so Join https://t.me/IntellivoidDev and send #activateapi and follow instructions.
-			This Module also Needs MongoDB For a Storage of Some Data So make Sure you have that too.
+			This Module also Needs MongoDB For Storage of Some Data So make Sure you have that too.
 
 Usage: .cf <as a reply to user's message //Turns AI on For that user.
 	   .delcf <as a reply to user's message //Turns AI off For that user.			
+	   .listcf //Outputs List Of Currently added Users in AI Auto-Chat.
 
 Credits: @Zero_cool7870 (For Writing This Module)
 		 Zi Xing (For CoffeeHouse API)			
@@ -23,22 +24,26 @@ from telethon import events
 from pymongo import MongoClient
 logging.basicConfig(level=logging.INFO)
 
-api_key = Config.LYDIA_API
+if Config.MONGO_URI and Config.LYDIA_API is not None:
+	api_key = Config.LYDIA_API
 
-# Initialise client
-api_client = cf.API(api_key)
-cl = MongoClient(Config.MONGO_URI)
-db = cl['test']
-lydia = db.lydia
+	# Initialise client
+	api_client = cf.API(api_key)
+	cl = MongoClient(Config.MONGO_URI)
+	db = cl['test']
+	lydia = db.lydia
 
 
 @borg.on(admin_cmd(pattern="cf", allow_sudo=True))
-async def lydia_search(event):
+async def lydia_enable(event):
 	if event.fwd_from:
 		return
+	if Config.MONGO_URI and Config.LYDIA_API is None:
+		await event.edit("Make Sure You've added MONGO_URI and LYDIA_API env vars Correctly.")	
+		return		
 	if event.reply_to_msg_id == None:
 		await event.edit("Reply To A User's Message to Add him/her in Lydia Auto-Chat.")
-		return	 	
+		return		 	
 	reply_msg = await event.get_reply_message()	
 	user_id = reply_msg.from_id
 	chat_id = event.chat_id
@@ -58,9 +63,12 @@ async def lydia_search(event):
 	await event.edit("Lydia AI Turned On for User: "+str(user_id))
 
 @borg.on(admin_cmd(pattern="delcf", allow_sudo=True))
-async def lydia_search(event):
+async def lydia_disable(event):
 	if event.fwd_from:
 		return
+	if Config.MONGO_URI and Config.LYDIA_API is None:
+		await event.edit("Make Sure You've added MONGO_URI and LYDIA_API env vars Correctly.")	
+		return		
 	if event.reply_to_msg_id == None:
 		await event.edit("Reply To A User's Message to Remove him/her from Lydia Auto-Chat.")
 		return	 		 
@@ -73,12 +81,29 @@ async def lydia_search(event):
 		if c['user_id'] == user_id and c['chat_id'] == chat_id:
 			lydia.delete_one(c)
 	await event.edit("Lydia AI Turned OFF for User: "+str(user_id))			
-	
 
+@borg.on(admin_cmd(pattern="listcf", allow_sudo=True))
+async def lydia_list(event):
+	if event.fwd_from:
+		return
+	if Config.MONGO_URI and Config.LYDIA_API is None:
+		await event.edit("Make Sure You've added MONGO_URI and LYDIA_API env vars Correctly.")	
+		return		
+	await event.edit("Processing...")
+	msg = "**Auto-Chat:**"
+	cur = lydia.find({})
+	for c in cur:
+		user_id = str(c['user_id'])
+		msg += "\n__User:__ [{}](tg://user?id={})".format(user_id,user_id)
+		msg += "\n__Chat:__ `{}`\n".format(str(c['chat_id']))	
+	await event.edit(msg)	
+	
 @borg.on(events.NewMessage())  
 async def Lydia_bot_update(event):
 	if event.fwd_from:
 		return
+	if Config.MONGO_URI and Config.LYDIA_API is None:
+		return		
 	if event.media == None:	
 		cursor = lydia.find({})
 		for c in cursor:
