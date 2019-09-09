d7c58f902792c542223fb5285bbfc237e81ad5c1
jaskaranSM <37726998+jaskaranSM@users.noreply.github.com>
2019-09-09 18:50:40 +0530

    Config: Another Derp Fix

--- sample_config.py
+++ sample_config.py
@@ -100,7 +100,7 @@ class Config(object):
     VERY_STREAM_LOGIN = os.environ.get("VERY_STREAM_LOGIN", None)
     VERY_STREAM_KEY = os.environ.get("VERY_STREAM_KEY", None)
     TEMP_DIR = os.environ.get("TEMP_DIR", None)
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID"))
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", 1234))
     #MongoDB
     MONGO_URI = os.environ.get("MONGO_URI", None)
     #Lydia API
