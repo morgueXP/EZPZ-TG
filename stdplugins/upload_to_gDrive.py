8a77826312c076a4075e6b745a1076b3266ae48b
jaskaranSM <37726998+jaskaranSM@users.noreply.github.com>
2019-09-21 09:40:47 -0400

    gdrive: Increase Sleep Value to prevent Floodwaits while Uploading large Files.

--- stdplugins/upload_to_gDrive.py
+++ stdplugins/upload_to_gDrive.py
@@ -290,7 +290,7 @@ async def upload_file(http, file_path, file_name, mime_type, event, parent_id):
     display_message = ""
     while response is None:
         status, response = file.next_chunk()  #Credits: https://github.com/AvinashReddy3108/PaperplaneExtended/commit/df65da55d16a6563aa9023cac2bedf43248379f5
-        await asyncio.sleep(1)
+        await asyncio.sleep(5)
         if status:
             percentage = int(status.progress() * 100)
             progress_str = "[{0}{1}]\nProgress: {2}%\n".format(
