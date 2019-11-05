b71539700e620da1bef73f807c55cafc01deeafa
Dc5000 <chauhan.divanshu@gmail.com>
2019-11-05 18:49:00 +0530

    Update carbon.py

--- stdplugins/carbon.py
+++ stdplugins/carbon.py
@@ -63,7 +63,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Made using [Carbon](https://carbon.now.sh/about/), a project by [Dawn Labs](https://dawnlabs.io/)",
+         caption="Made with Love by [DraX](https://t.me/DraXCommunity)",
          force_document=True,
          reply_to=e.message.reply_to_msg_id,
          )
