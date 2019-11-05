5478cf102ce3b990f3693c36445f126ba13e5170
Dc5000 <chauhan.divanshu@gmail.com>
2019-11-05 23:01:14 +0530

    Update promote_demote.py

--- stdplugins/promote_demote.py
+++ stdplugins/promote_demote.py
@@ -14,14 +14,14 @@ async def _(event):
     start = datetime.now()
     to_promote_id = None
     rights = ChatAdminRights(
-        change_info=True,
+        change_info=None,
         post_messages=True,
         edit_messages=True,
         delete_messages=True,
         ban_users=True,
         invite_users=True,
         pin_messages=True,
-        add_admins=True,
+        add_admins=None,
     )
     input_str = event.pattern_match.group(1)
     reply_msg_id = event.message.id
@@ -44,14 +44,14 @@ async def _(event):
     start = datetime.now()
     to_demote_id = None
     rights = ChatAdminRights(
-        change_info=False,
-        post_messages=True,
+        change_info=True,
+        post_messages=None,
         edit_messages=True,
-        delete_messages=False,
-        ban_users=False,
-        invite_users=True,
-        pin_messages=False,
-        add_admins=False,
+        delete_messages=True,
+        ban_users=True,
+        invite_users=None,
+        pin_messages=True,
+        add_admins=True,
     )
     input_str = event.pattern_match.group(1)
     reply_msg_id = event.message.id
@@ -66,27 +66,3 @@ async def _(event):
         await event.edit(str(exc))
     else:
         await event.edit("Successfully Demoted")
-
-
-@borg.on(admin_cmd(pattern="prankpromote ?(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    start = datetime.now()
-    to_promote_id = None
-    rights = ChatAdminRights(
-        post_messages=True
-    )
-    input_str = event.pattern_match.group(1)
-    reply_msg_id = event.message.id
-    if reply_msg_id:
-        r_mesg = await event.get_reply_message()
-        to_promote_id = r_mesg.sender_id
-    elif input_str:
-        to_promote_id = input_str
-    try:
-        await borg(EditAdminRequest(event.chat_id, to_promote_id, rights, ""))
-    except (Exception) as exc:
-        await event.edit(str(exc))
-    else:
-        await event.edit("Successfully Promoted")
