28a28c5f5414d1730db8f1f3e3d85ac85dba72ae
Dc5000 <chauhan.divanshu@gmail.com>
2019-11-07 07:00:03 +0530

    Update time.py

--- stdplugins/time.py
+++ stdplugins/time.py
@@ -1,5 +1,5 @@
 """ It does not do to dwell on dreams and forget to live
-Syntax: .getime"""
+Syntax: .time"""
 
 import asyncio
 import os
@@ -11,11 +11,11 @@ from uniborg.util import admin_cmd
 FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
 
 
-@borg.on(admin_cmd("getime ?(.*)"))  # pylint:disable=E0602
+@borg.on(admin_cmd("time ?(.*)"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
-    current_time = datetime.now().strftime("%H : %M : %S")
+    current_time = datetime.now().strftime("⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡ \n⚡DraX TIMEZØNE⚡ \n LOCATION: INDIA \n  Time: %H:%M:%S \n  Date: %d.%m.%y \n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡")
     start = datetime.now()
     input_str = event.pattern_match.group(1)
     reply_msg_id = event.message.id
@@ -28,7 +28,7 @@ async def _(event):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)  # pylint:disable=E0602
     # pylint:disable=E0602
     required_file_name = Config.TMP_DOWNLOAD_DIRECTORY + " " + str(datetime.now()) + ".webp"
-    img = Image.new("RGB", (250, 50), color=(0, 0, 0))
+    img = Image.new("RGBA", (350, 220), color=(0, 0, 0, 115))
     fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
     drawn_text = ImageDraw.Draw(img)
     drawn_text.text((10, 10), current_time, font=fnt, fill=(255, 255, 255))
@@ -36,19 +36,19 @@ async def _(event):
     await borg.send_file(  # pylint:disable=E0602
         event.chat_id,
         required_file_name,
-        caption="Time: Powered by @UniBorg",
+        caption="Userbot: Powered by @DraXCommunity",
         # Courtesy: @ManueI15
         reply_to=reply_msg_id
     )
     os.remove(required_file_name)
     end = datetime.now()
     time_taken_ms = (end - start).seconds
-    await event.edit("Created sticker in {} seconds".format(time_taken_ms))
+    await event.edit("Created sticker in {} seconds, aare maa chuda na bc".format(time_taken_ms))
     await asyncio.sleep(5)
     await event.delete()
 
 
-@borg.on(admin_cmd("time (.*)"))  # pylint:disable=E0602
+@borg.on(admin_cmd("gtime (.*)"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
