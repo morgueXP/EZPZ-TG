fdd257e27b8c3e6e017430c07d710949179ec751
Dc5000 <chauhan.divanshu@gmail.com>
2019-11-09 11:12:37 +0530

    Update auto_profile.py

--- stdplugins/auto_profile.py
+++ stdplugins/auto_profile.py
@@ -12,7 +12,7 @@ from uniborg.util import admin_cmd
 DEL_TIME_OUT = 70
 
 
-@borg.on(admin_cmd(pattern="autobio"))  # pylint:disable=E0602
+@borg.on(admin_cmd(pattern="enautobio"))  # pylint:disable=E0602
 async def _(event):
     await event.reply("Turned on Auto-Bio")
     if event.fwd_from:
@@ -36,11 +36,23 @@ async def _(event):
             #     "Successfully Changed Profile Bio"
             # )
         await asyncio.sleep(DEL_TIME_OUT)
-        
-'''async def handler(event):
-    await event.reply("Turned on Auto-Bio")'''
 
-@borg.on(admin_cmd(pattern="autoname"))  # pylint:disable=E0602
+        
+@borg.on(admin_cmd(pattern="disautobio"))  # pylint:disable=E0602
+async def _(event):
+    await event.reply("Turned off Auto-Bio")
+    if event.fwd_from:
+        return
+    while True:
+        bio = f"@SkuzzyWorld Don't mess with me"
+        logger.info(bio)
+        try:
+            await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
+                about=bio
+            ))
+            
+            
+@borg.on(admin_cmd(pattern="enautoname"))  # pylint:disable=E0602
 async def _(event):
     await event.reply("Turned on Auto-Name")
     if event.fwd_from:
@@ -65,5 +77,18 @@ async def _(event):
             # )
         await asyncio.sleep(DEL_TIME_OUT)
 
-'''async def handler(event):
-    await event.reply("Turned on Auto-Bio")'''
+
+@borg.on(admin_cmd(pattern="edisautoname"))  # pylint:disable=E0602
+async def _(event):
+    await event.reply("Turned on Auto-Name")
+    if event.fwd_from:
+        return
+    while True:
+        DM = time.strftime("%d.%m.%y")
+        HM = time.strftime("%H:%M")
+        name = f"IN Skuzzy IN"
+        logger.info(name)
+        try:
+            await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
+                first_name=name
+            ))
