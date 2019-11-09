edff447f8e58f8fcb81cb7f3b882363ee9683006
Dc5000 <chauhan.divanshu@gmail.com>
2019-11-09 11:22:44 +0530

    Update repo.py

--- stdplugins/repo.py
+++ stdplugins/repo.py
@@ -1,6 +1,6 @@
 from telethon import events
 from uniborg.util import admin_cmd
 
-@borg.on(admin_cmd(repo"))
+@borg.on(admin_cmd("repo"))
 async def handler(event):
     await event.edit("[Click here](https://github.com/Dc5000/DraXHub) to open this lit repo!")
