fc8c83bb92b2318618e15ac96d3423dbda04c5c8
Dc5000 <chauhan.divanshu@gmail.com>
2020-01-11 17:42:00 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -115,14 +115,10 @@ class Config(object):
     # Google Drive ()
     G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
     G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
-    G_DRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
-    G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("AUTH_TOKEN_DATA", None)
-    if G_DRIVE_AUTH_TOKEN_DATA != None:
-        os.makedirs(TMP_DOWNLOAD_DIRECTORY)
-        t_file = open(TMP_DOWNLOAD_DIRECTORY+"auth_token.txt","w")
-        t_file.write(G_DRIVE_AUTH_TOKEN_DATA)
-        t_file.close()
-
+    G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
+    #
+    TELE_GRAM_2FA_CODE = os.environ.get("TELE_GRAM_2FA_CODE", None)
+    #
 
 class Production(Config):
     LOGGER = False
