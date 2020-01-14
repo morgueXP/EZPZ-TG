d3c20875b00c752cd54695a6dcc03a9b24f7b8e9
Dc5000 <chauhan.divanshu@gmail.com>
2020-01-14 23:26:49 +0530

    Update fban.py

--- stdplugins/fban.py
+++ stdplugins/fban.py
@@ -10,17 +10,8 @@ from uniborg.util import admin_cmd
 
 @borg.on(admin_cmd("fban ?(.*)"))
 async def _(event):
-    if event.fwd_from:
-        return
-    reason = event.pattern_match.group(1)
-    if event.reply_to_msg_id:
-        r = await event.get_reply_message()
-        if r.forward:
-            r_from_id = r.forward.from_id or r.from_id
-        else:
-            r_from_id = r.from_id
-        await borg.send_message(
-            Config.F_BAN_LOGGER_GROUP,
+    await borg.send_message(
+        Config.F_BAN_LOGGER_GROUP,
             "/fban {}".format(reason)
         )
     await event.edit("Fbanned User Successfully")
@@ -28,14 +19,8 @@ async def _(event):
 
 @borg.on(admin_cmd("unfban ?(.*)"))
 async def _(event):
-    if event.fwd_from:
-        return
-    reason = event.pattern_match.group(1)
-    if event.reply_to_msg_id:
-        r = await event.get_reply_message()
-        r_from_id = r.from_id
-        await borg.send_message(
-            Config.F_BAN_LOGGER_GROUP,
+    await borg.send_message(
+        Config.F_BAN_LOGGER_GROUP,
             "/unfban {}".format(reason)
         )
     await event.edit("Unfbanned User Successfully")
