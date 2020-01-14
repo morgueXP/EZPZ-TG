c07cfe7a68d5e5a8775a29892b6ea5aecec2e77a
Dc5000 <chauhan.divanshu@gmail.com>
2020-01-15 00:34:30 +0530

    Update fban.py

--- stdplugins/fban.py
+++ stdplugins/fban.py
@@ -6,16 +6,24 @@ Available Commands:
 from telethon import events
 import asyncio
 from uniborg.util import admin_cmd
-
+from telethon.tl import functions, types
 
 @borg.on(admin_cmd("fban ?(.*)"))
 async def _(event):
     reason = event.pattern_match.group(1)
-    await borg.send_message(
-        Config.F_BAN_LOGGER_GROUP,
-            "/fban {reason}"
+    if reason:
+        await event.edit(f"Fbanned User Successfully")
+    else:
+        await event.edit(f"Fbanned that prick")
+    await asyncio.sleep(5)
+    await event.delete()
+    try:
+        await borg.send_message(  # pylint:disable=E0602
+            Config.F_BAN_LOGGER_GROUP,  # pylint:disable=E0602
+            f"/fban {reason}"
         )
-    await event.edit("Fbanned User Successfully")
+    except Exception as e:  # pylint:disable=C0103,W0703
+        logger.warn(str(e))  # pylint:disable=E0602
 
 
 @borg.on(admin_cmd("unfban ?(.*)"))
